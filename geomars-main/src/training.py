04e8907a338ce610e6827b8cd850a9ce2be8985d
Niklas Sprengel <niklas.sprengel@campus.tu-berlin.de>
2021-05-13 15:24:43 +0200

    Fix

--- geomars-main/src/training.py
+++ geomars-main/src/training.py
@@ -8,7 +8,7 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from models import MarsModel
 
 hyper_params = {
-    "batch_size": 32,
+    "batch_size": 16,
     "num_epochs": 1,
     "learning_rate": 1e-2,
     "optimizer": "sgd",
@@ -53,5 +53,7 @@ model = MarsModel(hyper_params)
 trainer = pl.Trainer(
     gpus=1, max_epochs=hyper_params["num_epochs"], checkpoint_callback=checkpoint
 )
-trainer.fit(model, train_dataloader=train_loader, val_dataloaders=val_loader)
-trainer.test(test_dataloaders=test_loader)
+
+if __name__ == '__main__':
+    trainer.fit(model, train_dataloader=train_loader, val_dataloaders=val_loader)
+    trainer.test(test_dataloaders=test_loader)
