f4b03dafd62fb48d5c186f83c9313b8c4f07c6ba
Niklas Sprengel <niklas.sprengel@campus.tu-berlin.de>
2021-06-30 15:33:43 +0200

    Fixed imports

--- geomars-pytorch/data.py
+++ geomars-pytorch/data.py
@@ -6,6 +6,9 @@ import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
 from torchvision import transforms, datasets
+from sklearn.cluster import MiniBatchKMeans as KMeans
+import numpy as np
+import cv2
 
 
 def removeclassdoublings(indices_tuple, labels):
